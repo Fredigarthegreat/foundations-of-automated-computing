b103b6134f8cf6654eb23439e2d6dd7c0e3a0a7d
Fredigarthegreat <jormandpiano@gmail.com>
2025-10-04 14:59:30 -0700

    Basic content loading finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-const COLS = 83;
-const ROWS = 22;
+const COLS = 81;
+const ROWS = 25;
 const SCROLL_AMT_TO_MOVE = 30;
 
 function resize () {
@@ -10,37 +10,6 @@ function resize () {
     window.innerHeight / 1080,
   );
   wrapper.style.transform = `translate(-50%, -50%) scale(${scale})`;
-}
-
-
-function navPopulate(nav) {
-  var curHeight = 0;
-  while (nav.list.firstChild) {
-    nav.list.removeChild(nav.list.firstChild);
-  }
-  for (var i = nav.top; curHeight < nav.height; i++) {
-    if (i == nav.chapters.length) {
-      break;
-    }
-    const li = document.createElement("li");
-    li.setAttribute("tabindex", "0");
-    li.setAttribute("index", i);
-    if (i == nav.active) {
-      li.classList.add("selected");
-    }
-    const chapter = nav.chapters[i];
-      console.log(`curHeight + chapter.tHeight: ${curHeight + chapter.tHeight} `);
-      console.log("nav.height: " + nav.height);
-    if (!(curHeight + chapter.tHeight >= nav.height)) {
-      li.classList.add("full-margin");
-    }
-    if (curHeight + chapter.tHeight >= nav.height + 1) {
-      break;
-    }
-    li.innerText = chapter.title;
-    nav.list.append(li);
-    curHeight += chapter.tHeight + 1;
-  } - 1
 }
 
   function HandleScroll (e, elCont, callback) {
@@ -55,14 +24,97 @@ function navPopulate(nav) {
     }
   }
 
+function navPopulate(navState) {
+  var curHeight = 0;
+  while (navState.list.firstChild) {
+    navState.list.removeChild(navState.list.firstChild);
+  }
+  for (var i = navState.top; curHeight < navState.height; i++) {
+    if (i == navState.chapters.length) {
+      break;
+    }
+    const li = document.createElement("li");
+    li.setAttribute("tabindex", "0");
+    li.setAttribute("index", i);
+    if (i == navState.selectedChapter) {
+      li.classList.add("selected");
+    }
+    const chapter = navState.chapters[i];
+    if (!(curHeight + chapter.tHeight >= navState.height)) {
+      li.classList.add("full-margin");
+    }
+    if (curHeight + chapter.tHeight >= navState.height + 1) {
+      break;
+    }
+    li.innerText = chapter.title;
+    navState.list.append(li);
+    curHeight += chapter.tHeight + 1;
+  } - 1
+}
+
+function getActiveChapterLength (navState, material) {
+  const chapter = material[navState.chapters[navState.selectedChapter]["id"]];
+  if (chapter) {
+    return chapter.length;
+  } else {
+    return 0;
+  }
+}
+
+function navSelectPage (navState, material, pageIndex) {
+  console.log(material[0].length);
+  const chapterLength = getActiveChapterLength(navState, material);
+  if (0 <= pageIndex && pageIndex <= chapterLength - 1) {
+    navState.selectedPage = pageIndex;
+    navDrawPageControl(navState, material);
+  }
+}
+
+function articleOveride (navState, material, chapterID, pageIndex) {
+  const article = document.getElementById("article");
+  article.innerHTML = getChapterContent(navState, material, chapterID, pageIndex);
+}
+
+function getChapterContent(navState, material, chapterID, pageIndex) {
+  return material[chapterID][pageIndex];
+
+}
+
+
+function navDrawPageControl (navState, material) {
+  const chapterLength = getActiveChapterLength(navState, material);
+  var pageControlString = "";
+  for (var i = 0; i <= chapterLength - 1; i++) {
+    if (i == navState.selectedPage) {
+      pageControlString += "*";
+    } else {
+      pageControlString += "-";
+    }
+  }
+  navState.pageControl.innerText = pageControlString;
+  const article = document.getElementById("article");
+  if (chapterLength) {
+    article.innerHTML = 
+      getChapterContent(
+        navState, 
+        material, 
+        navState.chapters[navState.selectedChapter]["id"],
+        navState.selectedPage
+      );
+  } else {
+    article.innerHTML = "";
+  }
+}
 
 const Nav = {
   element: document.getElementById("nav"),
   list: document.getElementById("list"),
+  pageControl: document.getElementById("page-control"),
   height: 24,
   scrollBuf: 0,
   top: 0,
-  active: 0,
+  selectedChapter: 0,
+  selectedPage: 0,
   chapters: [
     { title: "01 Introduction", tHeight: 1, id : 0},
     { title: "chapter 2 \n line 2", tHeight: 2 },
@@ -84,77 +136,201 @@ const Nav = {
   ]
 };
 
-const content = {
-  0: "hello this is html"
+const courseMaterial = {
+  "help": [
+`            HELP
+
+         h: previous page
+         l: next page
+    j or n: next chapter
+    k or p: previous chapter
+            
+            Press ESC to exit help.`
+  ],
+  0: [
+`Hello! Thank you so much for visiting my website!
+
+I am so fired up to help you get started on your journey towards computer science
+mastery! It's a tricky field to get into, so I am very pleased you are trusting 
+my ability to help you find your way there. 
+
+This course is designed for complete beginners, but if you've already started
+programming, ESPECIALLY if you've found yourself discouraged or jaded after 
+your past incursions, this is definitely the right course for you. Even if 
+you are a seasoned amateur, I 100% guarantee that you will come away with a 
+new perspective, and you will likely learn something you didn't know before!
+
+If you're eager to get going, you can feel free to skip to the next chapter. 
+Or don't! Up to you...
+
+(Press the right arrow-key or l to go to the next page. Press ? for help)`,
+`Computers are an integral part of our everyday lives at the time of writing (the 
+year 2025). Everyone loves the improved quality of life that they offer. But most 
+people, even programmers, view computers as hopelessly complicated and aren't 
+encouraged to learn much about how they really work. So why should you as someone 
+interested in computer programming pay attention to how computers really work at
+a deep level?
+
+1. Computers are dope.
+
+This is my favorite reason! It is so wild to me that we have figured out how to 
+outsource our brain-work to components made from raw materials. I don't know 
+about you, but I get a unique exhilaration from gaining the ability to wrap my 
+mind around a complicated system that has the potential to be really useful. It's 
+especially rewarding when you find hidden tricks and thinking tools to understand 
+parts of it that other people have missed! I have a deep love for digital 
+computing that goes beyond providing value to computer users and I'm so excited 
+to share it with you! I already know that we're going to have a blast together! 
+
+(Press the left arrow-key or j if you want to go back to a previous page)`,
+`2. Bolster your employment chances.
+
+If you need income right away it might be wise to fast track your entry into the 
+industry by skipping right to web programming. But the dependability of this 
+choice is fading away quickly. It is getting harder and harder even for computer 
+science graduates to find employment, let alone junior developers fresh out of 
+web-dev boot camp. At the time of writing, the industry is in a widespread hiring 
+slump. You'll need to set yourself apart if you want a chance at getting hired. 
+
+3. Initiate a pattern for motivated growth.
+
+Does it seem impossible that a journey in computer science could exceed the 
+appeal of videogames or doom-scrolling? Somehow I've managed to achieve this 
+myself in spite of being a long-time pc gamer (nothing against videogames, by the 
+way, I love them!). At a minimum, you've probably spent the last 12 years of your 
+life learning stuff that you didn't find very important. I'm going to help you 
+plant your feet firmly on the ground so you won't have to feel like you're 
+floating along anymore. For the first time (maybe), we are going to unlock your 
+internal motivation to grow in personal competence. Keep reading! `,
+`Who is this course for?
+
+Really anyone who thinks computers are cool and that they might want to do more 
+with them, maybe even start a career around them. Like I said before, I've made 
+this course for the average person. I do assume at certain points that you have 
+basic knowledge of how to use a computer. If you find yourself stuck, don't 
+hesitate to ask ChatGPT or a similar LLM for help. 
+
+I take a different point of view from mainstream computer programming education. 
+It's worth it to spend more time on the front end laying the foundation that you 
+will continue to build on for the rest of your career. Worried that computer 
+science might not be for you? Don't worry, you'll find out pretty quickly if 
+computer science is something that you want to progress further in.  Don't be 
+fooled though! There is a cost to laying a solid foundation. The cost is time. 
+It's worth it, but there is a cost. If you're life is unstable at the moment, you 
+have an uphill battle in front of you! Try to nail down an income source and get 
+to a point where you can "turtle-up" and give yourself the solid start in 
+computer science that you need. `,
+`If you're a highschooler or college-age student in a stable living situation, 
+fantastic! Take full advantage of the opportunity your parents gave you and use 
+your free time to build the most robust foundation that you can. And here, get 
+this: I'm going to help you unlock the fun in computer science. We're going to 
+have a blast!
+
+If you would, please fill out this survey. I want to know more about the people 
+who are checking out my educational material.
+
+And with that, let's get cooking!`,
+  ]
 };
 
-function navMakeActive (chapterIndex) {
-  if (0 <= chapterIndex && chapterIndex <= Nav.chapters.length - 1) {
-    Nav.active = chapterIndex;
-    if (Nav.active < Nav.top + 1) {
-      Nav.top--;
-      if (Nav.top < 0) {
-        Nav.top = 0;
+function navSelectChapter (navState, material, chapterIndex) {
+  if (0 <= chapterIndex && chapterIndex <= navState.chapters.length - 1) {
+    navState.selectedChapter = chapterIndex;
+    if (navState.selectedChapter < navState.top + 1) {
+      navState.top--;
+      if (navState.top < 0) {
+        navState.top = 0;
       }
     }
-    const bottomIndex = Nav.top + Nav.list.childNodes.length - 1;
-    if (Nav.active > bottomIndex - 1) {
-      if (bottomIndex < Nav.chapters.length - 1) {
-        Nav.top++;
+    const bottomIndex = navState.top + navState.list.childNodes.length - 1;
+    if (navState.selectedChapter > bottomIndex - 1) {
+      if (bottomIndex < navState.chapters.length - 1) {
+        navState.top++;
       }
     }
-    navPopulate(Nav);
-    Nav.list.querySelectorAll("li").forEach(function (li) {
-      if (parseInt(li.getAttribute("index")) == Nav.active) {
+    navPopulate(navState);
+    navState.list.querySelectorAll("li").forEach(function (li) {
+      if (parseInt(li.getAttribute("index")) == navState.selectedChapter) {
         li.classList.add("selected");
       }
       else {
         li.classList.remove("selected");
       }
     });
-    const article = document.getElementById("article");
-    article.innerHTML = content[Nav.chapters[Nav.active]["id"]];
+    navSelectPage(navState, material, 0);
+    navDrawPageControl(navState, material);
   }
 }
 
-Nav.list.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    navMakeActive(parseInt(e.target.getAttribute("index")));
-  }
-})
+function navInitializeUI (navState, material) {
+  navState.list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+      navSelectChapter(navState, material, parseInt(e.target.getAttribute("index")));
+    }
+  })
 
-const navButtons = document.getElementById("nav-bottom-buttons");
+  const navButtons = document.getElementById("nav-bottom-buttons");
+  navButtons.addEventListener("click", function (e) {
+    if (e.target.id == "nav-down") {
+      navSelectChapter(navState, material, navState.selectedChapter + 1);
+    }
+    if (e.target.id == "nav-up") {
+      navSelectChapter(navState, material, navState.selectedChapter - 1);
+    }
+  })
 
-navButtons.addEventListener("click", function (e) {
-  if (e.target.id == "nav-down") {
-    navMakeActive(Nav.active + 1);
-  }
-  if (e.target.id == "nav-up") {
-    navMakeActive(Nav.active - 1);
-  }
-})
+  navState.element.addEventListener("wheel", function (e) {
+    HandleScroll(e, navState, function (amount) {
+      const bottomIndex = navState.top + navState.list.childNodes.length - 1;
+      if (amount == -1 && navState.top != 0 || amount == 1 && bottomIndex < navState.chapters.length - 1) {
+        navState.top += amount;
+        navPopulate(navState);
+      }
+    });
+  });
 
-function navPositionActive() {
-  if (Nav.active < Nav.top + 1) {
-    Nav.top == Nav.top + 1
-  }
+  document.onkeydown = function (e) {
+    switch(e.code) {
+      case "KeyL":
+      case "ArrowRight":
+        navSelectPage(navState, material, navState.selectedPage + 1);
+        break;
+      case "KeyH":
+      case "ArrowLeft":
+        navSelectPage(navState, material, navState.selectedPage - 1);
+        break;
+      case "KeyP":
+      case "KeyK":
+        navSelectChapter(navState, material, navState.selectedChapter - 1);
+        break;
+      case "KeyJ":
+      case "KeyN":
+        navSelectChapter(navState, material, navState.selectedChapter + 1);
+        break;
+      case "Slash":
+        if (e.shiftKey) {
+          articleOveride(navState, material, "help", 0);
+        }
+        break;
+      case "Escape":
+        navSelectChapter(navState, material, navState.selectedChapter);
+        break;
+    }
+  };
 }
 
 function navScroll (amount) {
-  const bottomIndex = Nav.top + Nav.list.childNodes.length - 1;
-  if (amount == -1 && Nav.top != 0 || amount == 1 && bottomIndex < Nav.chapters.length - 1) {
-    Nav.top += amount;
-    navPopulate(Nav);
+  const bottomIndex = navState.top + navState.list.childNodes.length - 1;
+  if (amount == -1 && navState.top != 0 || amount == 1 && bottomIndex < navState.chapters.length - 1) {
+    navState.top += amount;
+    navPopulate(navState);
   }
 }
 
-function navHandleScroll(e) {
-  HandleScroll(e, Nav, navScroll);
-}
-
+navInitializeUI(Nav, courseMaterial);
 navPopulate(Nav);
 
 window.addEventListener('resize', resize);
 resize();
 
-Nav.element.addEventListener("wheel", navHandleScroll);
+navSelectPage(Nav, courseMaterial, 0);
